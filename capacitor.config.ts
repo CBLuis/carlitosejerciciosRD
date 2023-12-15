9be96b13bc3e83d068322441d2524ed90604f026
CBLuis <carlos.carpiob.est@uets.edu.ec>
2023-12-15 14:24:59 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carlitosejerciciosRD',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
